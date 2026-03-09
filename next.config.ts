c48aa89e5277fcebf82fce07e8bc1ead95fcd20f
dnslin <i@dnsl.in>
2026-03-09 14:06:46 +0800

    feat: 添加tsconfig.tsbuildinfo到.gitignore并更新next.config.ts中的allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   images: { unoptimized: true },
+  allowedDevOrigins: ["127.0.0.1:3000", "localhost:3000"],
 };
 
 export default nextConfig;
+
